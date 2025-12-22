ca8edd9661987cae1171ec30a09eb235d24c1643
bruno becker <brunomd6@hotmail.com>
2025-12-22 16:51:03 -0300

    first

---
+++ README.md
@@ -0,0 +1,9 @@
+
+## Naming Convention
+YYYY-MM-DD-competicao-adversario.yml
+
+
+
+docker build -t gremio-formacao -f docker/Dockerfile .
+
+
---
+++ scripts/generator.py
@@ -0,0 +1,20 @@
+import svgwrite
+import yaml
+
+with open("/app/lineups/11_01_2026 - Gremio vs .yml") as f:
+    jogo = yaml.safe_load(f)
+
+dwg = svgwrite.Drawing("formacao.svg", size=("800px", "1200px"))
+
+# campo
+dwg.add(dwg.rect(insert=(0,0), size=("800px","1200px"),
+                 fill="#1f7a1f"))
+
+for j in jogo["jogadores"]:
+    x = j["x"] * 8
+    y = 1200 - j["y"] * 12
+    dwg.add(dwg.circle(center=(x, y), r=18, fill="white"))
+    dwg.add(dwg.text(j["nome"], insert=(x, y+35),
+                     text_anchor="middle"))
+
+dwg.save()
